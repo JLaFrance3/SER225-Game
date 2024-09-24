94c84d05035648bbf949e6379fb23f4d094fd0e8
easwear <erin.swearingen@quinnipiac.edu>
2024-09-24 19:39:22 -0400

    new trigger dialogue

---
+++ src/Scripts/TestMap/TestScript.java
@@ -0,0 +1,62 @@
+package Scripts.TestMap;
+
+import java.util.ArrayList;
+
+import Level.Script;
+import ScriptActions.ChangeFlagScriptAction;
+import ScriptActions.ConditionalScriptAction;
+import ScriptActions.ConditionalScriptActionGroup;
+import ScriptActions.CustomRequirement;
+import ScriptActions.FlagRequirement;
+import ScriptActions.LockPlayerScriptAction;
+import ScriptActions.ScriptAction;
+import ScriptActions.TextboxScriptAction;
+import ScriptActions.UnlockPlayerScriptAction;
+
+public class TestScript extends Script {
+    @Override
+    public ArrayList<ScriptAction> loadScriptActions() {
+        ArrayList<ScriptAction> scriptActions = new ArrayList<>();
+        // scriptActions.add(new LockPlayerScriptAction());
+
+        scriptActions.add(new TextboxScriptAction() {{
+            addText("These flowers look nice.");
+            addText("What kind of flowers do you like most?", new String[] { "Tulips", "Roses" });
+        }});
+
+        scriptActions.add(new ConditionalScriptAction() {{
+            // Answer 1
+            addConditionalScriptActionGroup(new ConditionalScriptActionGroup() {{
+                addRequirement(new CustomRequirement() {
+                    @Override
+                    public boolean isRequirementMet() {
+                        int answer = outputManager.getFlagData("TEXTBOX_OPTION_SELECTION");
+                        return answer == 0;
+                    }
+                });
+
+                addScriptAction(new TextboxScriptAction() {{
+                    addText("Oh, I love tulips too!");
+                }});
+            }});
+
+            // Answer 2
+            addConditionalScriptActionGroup(new ConditionalScriptActionGroup() {{
+                addRequirement(new CustomRequirement() {
+                    @Override
+                    public boolean isRequirementMet() {
+                        int answer = outputManager.getFlagData("TEXTBOX_OPTION_SELECTION");
+                        return answer == 1;
+                    }
+                });
+                
+                addScriptAction(new TextboxScriptAction("Roses are pretty!"));
+            }});
+        }});
+
+        scriptActions.add(new ChangeFlagScriptAction("flowerBed", true));
+        // scriptActions.add(new UnlockPlayerScriptAction());
+
+        return scriptActions;
+    }
+}
