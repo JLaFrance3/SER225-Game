6716824c526750a69df6e220873f5fac70934e09
easwear <erin.swearingen@quinnipiac.edu>
2024-11-07 15:32:11 -0500

    removed unnecessary triggers

--- src/Maps/TownMap.java
+++ src/Maps/TownMap.java
@@ -180,8 +180,8 @@ public class TownMap extends Map {
         triggers.add(new Trigger(startAreaSign.x, startAreaSign.y, 32, 32, new StartAreaSign(),"startAreaSign"));
 
         //Old Men triggers 
-        triggers.add(new Trigger(getMapTile(40,102).getX(),getMapTile(40,102).getY(), 32, 32, new OldMan2Script(), "talkedToOldMan2"));
-        triggers.add(new Trigger(getMapTile(66,103).getX(),getMapTile(66,103).getY(), 32, 32, new OldMan3Script(), "talkedToOldMan3"));
+        //triggers.add(new Trigger(getMapTile(40,102).getX(),getMapTile(40,102).getY(), 32, 32, new OldMan2Script(), "talkedToOldMan2"));
+        //triggers.add(new Trigger(getMapTile(66,103).getX(),getMapTile(66,103).getY(), 32, 32, new OldMan3Script(), "talkedToOldMan3"));
 
         return triggers;
     }
