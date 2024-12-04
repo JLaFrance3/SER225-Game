273e29e5d57671fd04f96fa39925bfcfc4ce1a1a
Skhy <skhycodes@gmail.com>
2024-12-04 15:10:19 -0500

    Fixes on GameWindow

--- src/Engine/GameWindow.java
+++ src/Engine/GameWindow.java
@@ -16,7 +16,7 @@ public class GameWindow {
 	private GamePanel gamePanel;
 
 	public GameWindow() {
-		gameWindow = new JFrame("Game");
+		gameWindow = new JFrame("Thief of Visions");
 		gamePanel = new GamePanel();
 		gamePanel.setFocusable(true);
 		gamePanel.requestFocusInWindow();
